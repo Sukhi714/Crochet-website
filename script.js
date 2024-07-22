39b6aa680612f9c3b423a28669475e6a3a14ca42
Sukhi714 <166290774+Sukhi714@users.noreply.github.com>
2024-07-22 20:16:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.querySelector('.hamburger');
+  const navLinks = document.querySelector('.nav-links');
+
+  hamburger.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+  });
+});
+
+
+
+
+document.getElementById('feedbackForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    // Simulate form submission (e.g., via AJAX)
+  
+    // Display thank you message
+    // document.getElementById('thankYouMessage').classList.remove('hidden');
+  alert("Thank you for your feedback")
+    // Reset form
+    this.reset();
+  });
+
+
+
+
+  let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+};
+
+document.querySelectorAll('.image-slider img').forEach(images =>{
+    images.onclick = () =>{
+        var src = images.getAttribute('src');
+        document.querySelector('.main-home-image').src = src;
+    };
+});
+
+var swiper = new swiper(".review-slider", {
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop : true,
+    grabCursor: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+    },
+});
+
+
+  
